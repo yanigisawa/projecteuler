27446a1500586bde7825b01ffe55da4cbbef18cb
James Alexander <403820+yanigisawa@users.noreply.github.com>
2011-11-11 13:32:35 -0500

    Solved problem 37 _slow_

--- helpers/primes.py
+++ helpers/primes.py
@@ -3,6 +3,7 @@ def getPrimes():
     primes = []
     # Note: This will only return the first 1000 primes defined in the given text file
     with open("../datafiles/primes1.txt") as f:
+    #with open("../datafiles/10000.txt") as f:
         for line in f:
             stringList = line.split()
             intList = [int(x) for x in stringList]
@@ -10,4 +11,23 @@ def getPrimes():
                 primes.append(x)
     return primes
 
-
+def getPrimesForTruncation():
+    primes = getPrimes()
+    sievePrimes = []
+    for p in primes:
+        pstr = str(p)
+        if '0' in pstr:
+            continue
+        elif '5' in pstr[1:]:
+            continue
+        elif '2' in pstr[1:]:
+            continue
+        elif '1' == pstr[0:1]:
+            continue
+        elif int(pstr[0]) not in [2, 3, 5, 7]:
+            continue
+        elif int(pstr[-1]) not in [2, 3, 5, 7]:
+            continue
+        else:
+            sievePrimes.append(p)
+    return sievePrimes
--- problems/37.py
+++ problems/37.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
+#Found 11 truncatable primes
+#[23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 739397]
+#Sum 748317: 
+#
+#real	15m16.034s
+#user	15m14.801s
+#sys	0m1.173s
+
+
 import sys
 sys.path.append('../helpers')
 from primes import *
 from mathHelper import *
 
-primes = getPrimes()
+allPrimes = getPrimes()
+primes = getPrimesForTruncation()
 
 def isTruncatablePrime(x):
     if x <= 7: return False
@@ -14,19 +24,20 @@ def isTruncatablePrime(x):
 
     for i in range(len(xstr)):
         y = int(xstr[i:])
-        if y not in primes: 
+        if y not in allPrimes:
             return False
 
     for i in range(1, len(xstr) + 1):
         y = int(xstr[0:i])
-        if y not in primes:
+        if y not in allPrimes:
             return False
 
     return True
 
-def test():
+def prob37():
     count = 0
     trunc = []
+
     for p in primes:
         if p > 7 and isTruncatablePrime(p):
             count = count + 1
@@ -34,7 +45,6 @@ def test():
 
     print("Found %s truncatable primes" % count)
     print(trunc)
+    print("Sum %s: " % sum(set(trunc)))
 
-test()
-#primes = getPrimes()
-#print(isTruncatablePrime(7043))
+prob37()
