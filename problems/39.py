41f32d6f31f7fbbd5d5c1916a03ecc375746068d
James Alexander <403820+yanigisawa@users.noreply.github.com>
2011-11-18 15:48:04 -0500

    Solved problem 39

--- problems/39.py
+++ problems/39.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
+import math
+
 def isTriple(a, b, c):
     return ((a * a) + (b * b)) == (c * c)
 
-def test():
-    print("Triple: %s" % isTriple(20, 48, 52))
+def findSolutions(p):
+    combinations = 0
+    for a in range(p / 3):
+        for b in range(a + 1, (p - a) / 2):
+            c = (p - a) - b
+            if isTriple(a, b, c):
+                combinations += 1
 
-test()
+    return combinations
+
+def prob39():
+    max = 0
+    for n in range(1001):
+        m = findSolutions(n)
+        if m > max:
+            max = m
+            perim = n
+
+    print("Max(%s): %s" % (perim, max))
+
+prob39()
