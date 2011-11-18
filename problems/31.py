2ecb42507c8e5d7e3784217ade44edc7fe5770d4
James Alexander <403820+yanigisawa@users.noreply.github.com>
2011-11-18 09:10:57 -0500

    Refactoring problem 31 solution

--- problems/31.py
+++ problems/31.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python
 
-import time
 coins = [200, 100, 50, 20, 10, 5, 2, 1]
-#coins = [200, 100, 50, 20, 10]
 combinations = 0
-
 
 def findCombination(coinIndex, coinCount, purseTotal):
     global combinations
-    #if purseTotal > 200: return 0
-    if coinIndex > (len(coins) - 1): return 0
     if purseTotal == 200:
         combinations += 1
-        return 0
+        return
+    if coinIndex > (len(coins) - 1): return 
 
     while True: 
         coinValue = coins[coinIndex] * coinCount
         newPurseTotal = coinValue + purseTotal
-        if newPurseTotal == 200:
-            combinations += 1
-            findCombination(coinIndex + 1, 1, purseTotal)
-            break
-        elif newPurseTotal > 200:
+        if newPurseTotal >= 200:
+            findCombination(coinIndex + 1, 1, newPurseTotal)
             findCombination(coinIndex + 1, 1, purseTotal)
             break
         else:
             findCombination(coinIndex + 1, 1, newPurseTotal)
             coinCount += 1
-
-
 
 def prob31():
     findCombination(0, 1, 0)
@@ -39,8 +30,8 @@ prob31()
 #  $ time ./31.py 
 #  Found combinations: XXXXX
 #  
-#  real	0m2.142s
-#  user	0m2.128s
-#  sys	0m0.011s
+#  real 0m2.142s
+#  user 0m2.128s
+#  sys  0m0.011s
 #  $ 
 #  
