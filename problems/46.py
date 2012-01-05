f27371a421f451b3ec728ca39288746a7795e485
James Alexander <403820+yanigisawa@users.noreply.github.com>
2012-01-04 19:41:26 -0500

    Solving problem 46

--- problems/46.py
+++ problems/46.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python
 
+import sys
+sys.path.append('../helpers')
+import mathHelper
+import primes
+
+def isSumOfSquare(x):
+    allPrimes = primes.getPrimesBelow(x)
+    for p in allPrimes:
+        y =1
+        sos = p + 2 * y ** 2
+        while sos < x:
+            y += 1
+            sos = p + 2 * y ** 2
+        if sos == x:
+            return True
+
+    return False
+
 def prob46():
+    for i in range(1, 500000, 2):
+        if not mathHelper.isPrime(i) and not isSumOfSquare(i):
+            print("Answer: %s" % i)
+            return
 
 prob46()
