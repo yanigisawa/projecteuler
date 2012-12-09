010a43e0ecbb16c57f2bc83b44bd27a7b97c4930
James Alexander <403820+yanigisawa@users.noreply.github.com>
2012-12-09 12:18:37 -0500

    - Solving problem 51 (with help from the internet) - Fixed several bugs found in converting from c# to Python - The resulting python now performs the correct calculation to find the number in less than a second.

--- problems/51.py
+++ problems/51.py
@@ -2,155 +2,49 @@
 
 """
 Source code from: http://www.mathblog.dk/project-euler-51-eight-prime-family/
+First of all we are looking for an 8 member family. And since we are looking
+for the smallest member of the family our repeated digits has to be 0, 1 or 2. 
+Otherwise we wont be able to make an eight member family.
 
+Besides saying something about the size of the repeated digit we can also say
+something about the number of repeated digits in the family. 
+A number can be divided by 3 if the digit sum is is divisible by 3.  If we
+calculate the digit sum mod 3 of the repeated digits we get the following
+results with the number of repeated digits
 
-using System;
-using System.Diagnostics;
+Result	Number of repeated digits
+-	1	2	3	4	5
+0	4	4	10	4	4
+1	3	3	0	3	3
+2	3	3	0	3	3
+If the number we have 1 repeated digit then the result n%3 will be 0 4 times,
+namly for 0, 3, 6 and 9. It will be 1 a total of 3 times and 2 a total number
+of 3 times.
 
+That means that if we no matter what the digit sum of the non repeating digit
+we will get at most 7 primes except if we use 3 repeating digits (or a
+multiplum of three repeating digits). And the non repeating digits have a digit
+sum mod 3 different from 0.
 
-namespace euler {
-    class Problem51 {
+What more can be said is that a prime number always ends in 1,3, 7 or 9 for
+primes larger than 10. This excludes the last digit from being a repeated
+digit.
 
-        public static void Main(string[] args) {
-            new Problem51().BruteForce();
-        }
-       
-        public void BruteForce() {
-            Stopwatch clock = Stopwatch.StartNew();
+So now we know a bit more about the kind of prime we are looking for. I assume
+that the prime will be 5 or 6 digits. It must have 3 digit being 0,1 or 2
+excluding the last digit of the number.
 
-            int[][] fiveDigitPattern = get5digitPatterns();
-            int[][] sixDigitPattern = get6digitPatterns();
-            int result = int.MaxValue;
-            
-            for (int i = 11; i < 1000; i += 2) {
-                
-                //Only 1,3,7,9 are valid endings
-                if (i % 5 == 0) continue;
-                
-                int[][] patterns = (i < 100) ? 
-                    fiveDigitPattern : sixDigitPattern;
-
-                for(int j = 0; j <patterns.GetLength(0); j++){
-                    for(int k = 0; k <= 2; k++){
-
-                        //Don't generate candidates with leading zero
-                        if (patterns[j][0] == 0 && k == 0) continue;
-                        
-                        int[] pattern = fillPattern(patterns[j], i);
-                        int candidate = generateNumber(k, pattern);
-                                                
-                        if (candidate < result && isPrime(candidate)) {
-                            if (familySize(k, pattern) == 8)
-                                result = candidate;                                                            
-                            break;                            
-                        }
-                    }                   
-                }
-            }
-              
-            clock.Stop();
-            Console.WriteLine("First prime in the eight prime family is {0}", result);            
-            Console.WriteLine("Solution took {0} ms", clock.ElapsedMilliseconds);
-        }
-
-        private int familySize(int repeatingNumber, int[] pattern) {
-            int familySize = 1;
-            
-            for (int i = repeatingNumber + 1; i < 10; i++) {
-                if (isPrime(generateNumber(i, pattern))) familySize++;                
-            }
-
-            return familySize;
-        }
-
-        private int[] fillPattern(int[] pattern, int number) {
-            int[] filledPattern = new int[pattern.Length];            
-            int temp = number;
-
-            for (int i = filledPattern.Length - 1; 0 <= i; i--) {
-                if (pattern[i] == 1) {
-                    filledPattern[i] = temp % 10;
-                    temp /= 10;
-                } else {
-                    filledPattern[i] = -1;
-                }
-            }
-            return filledPattern;
-        }
-
-        private int generateNumber(int repNumber, int[] filledPattern) {
-            int temp = 0;
-            for (int i = 0; i < filledPattern.Length; i++) {
-                temp = temp * 10;
-                temp += (filledPattern[i] == -1) ? 
-                    repNumber : filledPattern[i];
-            }                        
-            return temp;
-        }
-
-        private int[][] get5digitPatterns() {
-            int[][] retVal = new int[4][];
-
-            retVal[0] = new int[]{1,0,0,0,1};
-            retVal[1] = new int[]{0,1,0,0,1};
-            retVal[2] = new int[]{0,0,1,0,1};
-            retVal[3] = new int[]{0,0,0,1,1};            
-
-            return retVal;
-        }
-
-        private int[][] get6digitPatterns() {
-            int[][] retVal = new int[10][];
-
-            retVal[0] = new int[] { 1, 1, 0, 0, 0, 1 };
-            retVal[1] = new int[] { 1, 0, 1, 0, 0, 1 };
-            retVal[2] = new int[] { 1, 0, 0, 1, 0, 1 };
-            retVal[3] = new int[] { 1, 0, 0, 0, 1, 1 };
-            retVal[4] = new int[] { 0, 1, 1, 0, 0, 1 };
-            retVal[5] = new int[] { 0, 1, 0, 1, 0, 1 };
-            retVal[6] = new int[] { 0, 1, 0, 0, 1, 1 };
-            retVal[7] = new int[] { 0, 0, 1, 1, 0, 1 };
-            retVal[8] = new int[] { 0, 0, 1, 0, 1, 1 };
-            retVal[9] = new int[] { 0, 0, 0, 1, 1, 1 };
-          
-            return retVal;
-        }
-
-
-        private bool isPrime(int n) {
-            if (n <= 1) return false;            
-            if (n == 2) return true;            
-            if (n % 2 == 0) return false;            
-            if (n < 9) return true;            
-            if (n % 3 == 0) return false;
-            
-            int counter = 5;
-            while ((counter * counter) <= n) {
-                if (n % counter == 0) return false;                
-                if (n % (counter + 2) == 0) return false;                
-                counter += 6;
-            }
-
-            return true;
-        }
-     
-    }
-}
+So lets make some code.
 
 """
 
-import sys
-sys.path.append("../helpers")
-from primes import MillerRabin
-
-    
 def get5digitPatterns():
     retVal = []
 
-    retVal.append([1,0,0,0,1])
-    retVal.append([0,1,0,0,1])
-    retVal.append([0,0,1,0,1])
-    retVal.append([0,0,0,1,1])
+    retVal.append([ 1, 0, 0, 0, 1 ])
+    retVal.append([ 0, 1, 0, 0, 1 ])
+    retVal.append([ 0, 0, 1, 0, 1 ])
+    retVal.append([ 0, 0, 0, 1, 1 ])
 
     return retVal
 
@@ -158,7 +52,6 @@ def get6digitPatterns():
     retVal = []
 
     retVal.append([ 1, 1, 0, 0, 0, 1 ]) 
-    retVal.append([ 1, 0, 1, 0, 0, 1 ])
     retVal.append([ 1, 0, 0, 1, 0, 1 ])
     retVal.append([ 1, 0, 0, 0, 1, 1 ])
     retVal.append([ 0, 1, 1, 0, 0, 1 ])
@@ -171,14 +64,15 @@ def get6digitPatterns():
     return retVal
     
 def fillPattern(pattern, number):
-    filledPattern, temp = [], number
+    temp = number
+    filledPattern = [0 for x in pattern]
 
     for i in range(len(pattern) - 1, -1, -1): # decrementing loop; -1 is step
         if (pattern[i] == 1): 
-            filledPattern.append(temp % 10)
+            filledPattern[i] = temp % 10
             temp = temp / 10
         else: 
-            filledPattern.append(-1)
+            filledPattern[i] = -1
     return filledPattern
 
 def generateNumber(repNumber, filledPattern):
@@ -186,7 +80,7 @@ def generateNumber(repNumber, filledPattern):
     for i in range(len(filledPattern)):
         temp = temp * 10
 
-        if filledPattern[0] == -1: 
+        if filledPattern[i] == -1: 
             temp += repNumber
         else:
             temp += filledPattern[i]
@@ -215,7 +109,19 @@ def isPrime(n):
 
     return True
 
+"""
+Since we will be replacing 3 digits in the number, we need to find the remaining
+single digits. Looping from 11 to a max of 999 will generate at most a 3 digit
+number to replace the 3 non-repeating digits in the resulting number; this number
+is represented by 'i' in the following loop
 
+'k' represents the possible repeating values of 0, 1, or 2, since all other 
+possibilities have been excluded since we can't possibly achieve an 8 number
+family with anything greater
+
+'result' is initialized as a number greater than the largest possible 6 digit
+value, since the resulting candidate number should be less than this value
+"""
 def problem51():
 
     fiveDigitPattern = get5digitPatterns()
@@ -232,7 +138,7 @@ def problem51():
         else:
             patterns = sixDigitPattern
 
-        for j in range(len(patterns[0]) - 1):
+        for j in range(len(patterns)):
             for k in range(3):
 
                 # Don't generate candidates with leading zero
@@ -240,7 +146,6 @@ def problem51():
 
                 pattern = fillPattern(patterns[j], i)
                 candidate = generateNumber(k, pattern)
-                print "Pattern: {0} - Candidate: {1}".format(patterns[j], candidate)
 
                 if (candidate < result and isPrime(candidate)):
                     if (familySize(k, pattern) == 8):
@@ -250,7 +155,3 @@ def problem51():
             
 
 problem51()
-#x = get5digitPatterns()
-#print x[1]
-#y = fillPattern(x[0], 15)
-#print "Candidate: {0}".format(generateNumber(2, y))
